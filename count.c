0c98cd7d2e85670e127f2d4810a2ba5da3da5208
SatyamAditya <60105476+SatyamAditya@users.noreply.github.com>
2020-10-01 15:08:26 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,31 @@
+/* C Program to count vowels and consonants in a String */
+
+#include <stdio.h>
+
+int main()
+{
+      char str[80];
+      char *s = str;
+      int vowels, consonants;
+      vowels = consonants = 0;
+
+      printf("\n Enter any String :  ");
+      gets(str);
+
+      while (*s!='\0')
+      {
+          if(*s == 'a' || *s == 'e' || *s == 'i' || *s == 'o' || *s == 'u' ||
+        *s == 'A' || *s == 'E' || *s == 'I' || *s == 'O' || *s == 'U')
+        {
+              vowels++;
+         }
+          else
+          if(*s!=' ')
+          {
+            consonants++;
+            }
+        s++;
+    }
+    
+      return 0;
+}
